4504995a926bc73d447153a136a7be5a4ab96929
Eric Dahl <eric.c.dahl@gmail.com>
2021-05-23 13:13:49 -0700

    fix test

--- lambda/items/get/item/main.py
+++ lambda/items/get/item/main.py
@@ -4,17 +4,16 @@ from boto3_type_annotations.dynamodb import ServiceResource
 from boto3.dynamodb.conditions import Key
 
 dynamodb : ServiceResource = boto3.resource('dynamodb')
-items = dynamodb.Table("items")
+items = dynamodb.Table('items')
 
 
 def handler(event, context):
-    print("Received event: " + json.dumps(event, indent=2))
+    print('Received event: ' + json.dumps(event, indent=2))
 
     key = event['pathParameters']['item']
-
     response = items.query(KeyConditionExpression=Key('id').eq(key))
     return response['Items']
 
 
 if __name__ == '__main__':
-    print(handler(0, 0))
+    print(handler({'pathParameters': {'item':'1'}}, 0))
